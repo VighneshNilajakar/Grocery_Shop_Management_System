1bc0fa6d16c14df2210aa051481d293ee9f703dd
Vighnesh Nilajakar <84033298+VighneshNilajakar@users.noreply.github.com>
2022-02-11 20:57:14 +0530

    Update MAIN_CODE.py
    Co-Authored-By: ShivduttBhadakwad <99489286+ShivduttBhadakwad@users.noreply.github.com>

--- MAIN_CODE.py
+++ MAIN_CODE.py
@@ -5,9 +5,9 @@ msg1 = "GROCERY MANAGEMENT SYSTEM"
 pmsg1 = msg1.center(120)
 print(pmsg1)
 print("CHOOSE FROM THE FOLLOWING OPTIONS :-")
-print("1. PRODUCT DETAILS\n2. CUSTOMER DETAILS")
+print("1. PRODUCT DETAILS\n2. CUSTOMER DETAILS\n3. MANUFACTURER DETAILS\n4. QUIT")
 try:
-    choice=int(input('ENTER YOUR CHOICE (1/2):- '))
+    choice=int(input('ENTER YOUR CHOICE (1/2/3/4):- '))
     if choice==1:
         print("1. PRODUCT DETAILS\n2. ADD PRODUCT'S DATA\n3. REMOVE PRODUCT'S DATA\n4. UPDATE PRODUCT'S DATA")
         try:
@@ -27,7 +27,7 @@ try:
                 prod_id=int(input("ENTER ID OF THE PRODUCT YOU WANT TO REMOVE :- "))
                 cur.execute("DELETE FROM PROD_DETAILS WHERE PROD_ID = {}".format(prod_id))
                 con.commit()
-                print("PRODUCT REMOVED SUCCESSFULLY")
+                print("PRODUCT'S DATA REMOVED SUCCESSFULLY")
             #elif prod_choice==4: Product Modification
              #   prod_id=int(input("ENTER ID OF THE PRODUCT YOU WANT TO REMOVE :- "))
               #  cur.execute("DELETE FROM PROD_DETAILS WHERE PROD_ID = {}".format(prod_id))
@@ -39,55 +39,55 @@ try:
             print("INVALID INPUT")
 
     elif choice==2:
-        print("<1>CUSTORMER DETAILS\n<2>ADD CUSTOMER\n<3>REMOVE CUSTOMER\n<4>UPDATE CUSTOMER")
+        print("1. CUSTORMER DETAILS\n2. ADD CUSTOMER'S DATA\n3. REMOVE CUSTOMER'S DATA\n4. UPDATE CUSTOMER'S DATA")
         try:
-            cust_choice=int(input('Enter your choice: '))
+            cust_choice=int(input("ENTER YOUR CHOICE (1/2):- "))
             if cust_choice==1:
                 cur.execute("SELECT * FROM CUST_DETAILS;")
                 cust_details=cur.fetchll()
                 for j in cust_details:
                     print(j)
             elif cust_choice==2:
-                cust_name=input('Enter customer name: ')
-                cust_adrs=input('Enter customer address: ')
-                cust_mno=int(input('Enter customer mobile number: '))
-                cur.execute("INSERT INTO CUST_DETAILS(CUST_NAME,CUST_ADRS,CUST_MNO) VALUES('{}','{}',{})".format(cust_name,cust_adrs,cust_mno))
+                cust_name=input("ENTER CUSTOMER'S NAME :- ")
+                cust_mno=int(input("ENTER CUSTOMER'S MOBILE NUMBER :-"))
+                cust_adrs=input("ENTER CUSTOMER'S ADDRESS :- ")
+                cur.execute("INSERT INTO CUST_DETAILS(CUST_NAME,CUST_MNO,CUST_ADRS) VALUES('{}','{}',{})".format(cust_name,cust_mno,cust_adrs))
                 con.commit()
-                print('customer added successfully')
+                print("CUSTOMER'S DATA ADDED SUCCESSFULLY")
   
             elif cust_choice==3:
-                cust_id=int(input('Enter customer id: '))
+                cust_id=int(input("ENTER CUSTOMER'S ID :- "))
                 cur.execute('DELETE FROM CUST_DETAILS WHERE CUST_ID={}'.format(cust_id))
                 con.cummit()
-                print('Customer removed successfully')
+                print("CUSTOMER'S DATA REMOVED SUCCESSFULLY")
 
             elif cust_choice==4:
-                print('WHAT YOU WANT TO UPDATE: ')
-                print('<1>CUSTOMER NAME\n<2>CUSTOMER ADDRESS\n<3>CUSTOMER_MOBILE NUMBER')
-                update_cust=int(input('Enter your choice: '))
-                which_cust=int(input('Enter customer id whose data you want to update: '))
+                print("WHAT YOU WANT TO UPDATE :- ")
+                print("1. CUSTOMER'S NAME\n2. CUSTOMER'S ADDRESS\n3. CUSTOMER'S MOBILE NUMBER")
+                update_cust=int(input("ENTER YOUR CHOICE (1/2/3):- "))
+                which_cust=int(input("ENTER CUSTOMER'S ID :- "))
                 if update_cust==1:
-                    cust_name=int(input('Enter the customer name to update: '))
+                    cust_name=int(input("ENTER CUSTOMER'S NAME :- "))
                     cur.execute("UPDATE CUST_DETAILS SET CUST_NAME = '{}' WHERE CUST_ID={}".format(cust_name,which_cust))
                     con.commit()
-                    print('Customer update successfully')
+                    print("CUSTOMER'S NAME UPDATED SUCCESSFULLY")
 
                 elif update_cust==2:
-                    cust_update_adrs=input('Enter the new address of the address: ')
+                    cust_update_adrs=input("ENTER CUSTOMER'S NEW ADDRESS :- ")
                     cur.execute('UPDATE CUST_DETAILS SET ')
                     con.commit()
-                    print("customer address updated successfully")
+                    print("CUSTOMER'S ADDRESS UPDATED SUCCESSFULLY")
 
 
                 elif update_cust==3:
-                    cust_update_mno=int(input('Enter the new address of the address: '))
+                    cust_update_mno=int(input("ENTER CUSTOMER'S NEW MOBILE NUMBER :- "))
                     cur.execute('UPDATE CUST_DETAILS SET ')
                     con.commit()
-                    print("customer mobile number updated successfully")
+                    print("CUSTOMER'S MOBILE NUMBER UPDATED SUCCESSFULLY")
             
             else:
-                print('Invalid input')
+                print('INVALID INPUT')
         except:
-            print('Invalid input')
+            print('INVALID INPUT')
 except:
-    print('Invalid input')
+    print('INVALID INPUT')
